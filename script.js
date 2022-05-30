78a9415521be398456e5d744a0fe6259b2114e4c
Camila Wolter <camila0wolter@gmail.com>
2022-05-30 20:28:57 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// criando tarefas e adicionando em lista ordenada
 const listaTarefas = document.querySelector('#lista-tarefas');
 
 function addTarefa() {
@@ -9,3 +10,12 @@ function addTarefa() {
     textoTarefa.value = '';
 }
 document.querySelector('#criar-tarefa').addEventListener('click', addTarefa);
+
+// colocar fundo cinza quando clicar
+function addCor(evento) {
+    const cor = evento.target;
+    const lista = document.querySelector('li');
+    cor.classList.add('selecionado');
+    cor.style.backgroundColor = 'grey'
+}
+listaTarefas.addEventListener('click', addCor);
